6a106d91715c05c3e17e68fb7b1439389210a064
Eddy0021 <53792992+Eddy0021@users.noreply.github.com>
2024-05-13 23:10:12 +0200

    Task 6 (SQS & SNS, Async Microservices Communication)

--- import-service/handlers/importFileParser.ts
+++ import-service/handlers/importFileParser.ts
@@ -2,11 +2,14 @@
 
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { Readable } from 'stream';
+import { SQSClient, SendMessageCommand, SendMessageBatchCommand } from '@aws-sdk/client-sqs';
 const csvParser = require('csv-parser');
 
 import { handleResponse } from '../utils/handleResponse';
 
 const s3Client = new S3Client();
+const sqsClient = new SQSClient();
+const sqsQueueUrl = process.env.SQSQUEUEURL;
 
 module.exports.importFileParserHandler = async (event) => {
   const records: any = [];
@@ -30,7 +33,7 @@ module.exports.importFileParserHandler = async (event) => {
     try {
       // Call getObject method and await the response
       const { Body } = await s3Client.send(new GetObjectCommand(getObjectParams));
-      
+
       if (!Body) {
         console.error('Object body is empty:', key);
         continue;
@@ -41,10 +44,11 @@ module.exports.importFileParserHandler = async (event) => {
 
       // Parse CSV using csv-parser
       const parserStream = s3Stream.pipe(csvParser());
-      
-      // Log each record to CloudWatch
-      parserStream.on('data', (data: any) => {
-        console.log('Parsed record:', data);
+
+      // Log each record to CloudWatch and add to records array
+      parserStream.on('data', (data) => {
+        data.price = parseInt(data.price);
+        //console.log('Parsed record:', data);
         records.push(data);
       });
 
@@ -61,6 +65,19 @@ module.exports.importFileParserHandler = async (event) => {
     } catch (error) {
       console.error('Error fetching object from S3:', error);
     }
+  }
+
+  try {
+    // Send the accumulated records as a batch to SQS
+    const params = {
+      QueueUrl: sqsQueueUrl,
+      MessageBody: JSON.stringify(records)
+    };
+
+    await sqsClient.send(new SendMessageCommand(params));
+    console.log('Records sent to SQS successfully.');
+  } catch (error) {
+    console.error('Error sending records to SQS:', error);
   }
 
   return handleResponse(200, JSON.stringify({ records }));
